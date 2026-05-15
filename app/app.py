77f6761f224857af6d36a3f2e6bf3c62f170dfa5
Sai Teja Reddy Battu <sai.battu@atyeti.com>
2026-05-15 10:31:30 +0530

    add new update

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from ECS Flasdk application kslak App sai and co co workers the help of the local!"
+    return "Hello from ECS Flask application!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
