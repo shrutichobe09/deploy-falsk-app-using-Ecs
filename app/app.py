014a70b7a83544c6a4696c9545e905633cffc32b
Sai Teja Reddy Battu <sai.battu@atyeti.com>
2026-05-18 10:25:26 +0530

    add a new upadte

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from ECS Flask application! with althaf ahamed and sai tejareddy"
+    return "Hello from ECS Flask application! with data i wanted 👌👌👌👌👌👌👌👌👌👌"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
