7d8bf2081b1898ebc3f698b870468c2f0da8d3fd
Sai Teja Reddy Battu <sai.battu@atyeti.com>
2026-05-18 10:13:20 +0530

    add new update push

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from ECS Flask application!"
+    return "Hello from ECS Flask application! with althaf ahamed and sai tejareddy"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
