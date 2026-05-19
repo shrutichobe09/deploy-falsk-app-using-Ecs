db286115b486333810f7e064b54dc4ad8857afe0
Sai Teja Reddy Battu <sai.battu@atyeti.com>
2026-05-19 18:30:55 +0530

    add a new idex.html

--- app/app.py
+++ app/app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from ECS Flask application! with data i wanted 👌👌👌👌👌👌👌👌👌👌 one ---->"
+    data = {
+        "title": "Flask Docker Application",
+        "message": "Hello from ECS Flask application!",
+        "emoji": "👌👌👌👌👌👌👌👌👌👌",
+        "author": "Sai Teja"
+    }
+
+    return render_template("index.html", data=data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
