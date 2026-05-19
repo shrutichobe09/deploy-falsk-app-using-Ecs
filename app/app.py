1a72fa6f6d443463246f1e9dc58f9fc726846793
Sai Teja Reddy Battu <sai.battu@atyeti.com>
2026-05-19 21:19:24 +0530

    updated the more replicas

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     data = {
         "title": "Flask Application",
-        "message": "Hello From the Waforge Team!",
+        "message": "Hello From the Waforge Team to users!",
         "emoji": "😍😍😍",
         "author": "Atyeti"
     }
